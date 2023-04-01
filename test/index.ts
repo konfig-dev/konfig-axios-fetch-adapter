124f23c7147e7fbf5097fb4470bafbec00d9c79d
dphuang2 <dylan.p.huang@gmail.com>
2023-04-01 00:09:51 -0700

    convert to ts

---
+++ test/index.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import fetchAdapter from "..";
+
+window["axios"] = axios;
+window.onload = async function () {
+  try {
+    const data = await axios.request({
+      url: "/package.json",
+      method: "get",
+      adapter: fetchAdapter,
+    });
+    document.getElementById("app")?.append(JSON.stringify(data, null, 4));
+  } catch (e) {
+    console.log(e);
+  }
+};
+const formElem = document.getElementById("formElem");
+if (formElem && formElem instanceof HTMLFormElement) {
+  formElem.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let response = await axios.request({
+      url: "https://httpbin.org/post",
+      method: "post",
+      data: new FormData(formElem),
+      adapter: fetchAdapter,
+    });
+
+    console.log(response);
+  };
+}
